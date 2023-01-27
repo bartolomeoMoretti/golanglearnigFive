04087776beb888cf24032f715a00c5db6ec054b0
Sergio Ponomarev <ponomarevesq@gmail.com>
2023-01-27 07:56:47 +0400

    first commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+
+    // fetcher = fetcher.New()
+
+    //processor = processor.New()
+
+    // consumer - получает события (при помощи fetcher и обрабатывает пр помощи processor)
+	// consumer.Start(fetcher, processor)
+}
